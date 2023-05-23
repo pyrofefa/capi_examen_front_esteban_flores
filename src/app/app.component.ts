a1e9308e9ed3d3107775287c0ed400be69992ef9
estebanCesave <francisco.flores@cesaveson.com>
2023-05-23 13:42:55 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'capi_examen_front_esteban_flores';
+}
